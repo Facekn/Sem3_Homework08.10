e0d3b03c32422059b78cb9307a9ed9c13a4e5991
Ilya Grigorev <facekn110388@gmail.com>
2022-10-15 01:09:15 +0300

    Четные кубы диапазона N

---
+++ Sem3_Homework4/Program.cs
@@ -0,0 +1,12 @@
+﻿ Console.Write("Введите конечное число диапазона N: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+double cub = 0;
+for (int i = 1; i <= num; i++)
+{
+cub = Math.Pow(i, 3);
+    if ((cub%10)%2==0) 
+    {
+        Console.Write(cub+ " ");
+    } 
+}
+    
