d8a34da6d9f6c2454573d65a781fe0b98fa056fe
Ilya Grigorev <facekn110388@gmail.com>
2022-10-14 23:11:15 +0300

    Возведение чисел диапозона N в куб

---
+++ Sem3_Homework1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите конечное число диапазона: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int i = 1;
+while (i<=a)
+{
+    
+        Console.WriteLine("Куб числа - " +i+ " = " +i*i*i);
+    i++;
+}
