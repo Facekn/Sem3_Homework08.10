5775f82516fcd2b00ef0bca82b521cf2ec34c822
Ilya Grigorev <facekn110388@gmail.com>
2022-10-15 00:25:28 +0300

    Произведение чисел в диапазоне

---
+++ Sem3_Homework3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите конечное число диапозона N: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+int result = 1;
+
+for (int index = 1; index <= num; index++)
+{
+    result = result*index;   
+}
+Console.Write("Произведение чисел диапозона N = "+result);
