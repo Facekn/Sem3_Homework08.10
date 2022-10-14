d3adbf5e9c26013d7a73e0b0d338fa137f9d9cb7
Ilya Grigorev <facekn110388@gmail.com>
2022-10-14 23:47:45 +0300

    Сумма цифр в числе

--- Sem3_Homework2/Program.cs
+++ Sem3_Homework2/Program.cs
@@ -7,4 +7,4 @@ while (number / 10 != 0 || number % 10 != 0)
     sum = sum + (number%10);
     number = number/10;   
 }
-Console.WriteLine("Сумма чисел в в переменной Number = " +sum);
+Console.WriteLine("Сумма чисел в переменной Number = " +sum);
